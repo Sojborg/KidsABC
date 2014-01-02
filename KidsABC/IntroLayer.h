5204ffe214e47116de8e1b0ebcea1a34991f070d
Jesper <sjk@Stigs-MacBook-Pro.local>
2014-01-02 21:40:39 +0100

    Initial Commit

---
+++ KidsABC/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  KidsABC
+//
+//  Created by Jesper on 02/01/14.
+//  Copyright Jesper 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ KidsABC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KidsABC
+//
+//  Created by Jesper on 02/01/14.
+//  Copyright Jesper 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
